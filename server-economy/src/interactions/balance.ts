983d1f88a75821808d6ca7ebc507d24ba202e06c
Brooklyn Blackshear <ablackshear7820@gmail.com>
2025-02-14 22:18:58 -0600

    Made balance message private

--- server-economy/src/interactions/balance.ts
+++ server-economy/src/interactions/balance.ts
@@ -1,11 +1,12 @@
-import { Interaction } from "discord.js";
+import { Interaction, MessageFlags } from "discord.js";
 import { checkBalance } from "src/lib/db";
 
 export async function handleBalance(interaction: Interaction) {
   if (!interaction.isCommand()) return;
 
   const balance = await checkBalance(interaction.user);
-  await interaction.reply(
-    `<@${interaction.user.id}>, you have ${balance} gold.`
-  );
+  await interaction.reply({
+    content: `<@${interaction.user.id}>, you have ${balance} gold.`,
+    flags: [MessageFlags.Ephemeral],
+  });
 }
